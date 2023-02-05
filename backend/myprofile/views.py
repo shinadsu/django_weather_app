e8c6a386a13cf28ead584c914d7b732054629d8f
shinadsu <69975772+shinadsu@users.noreply.github.com>
2023-02-05 14:40:56 +0300

    Update views.py

--- backend/myprofile/views.py
+++ backend/myprofile/views.py
@@ -37,7 +37,6 @@ class information_site_page(DataMixin, ListView):
         return context_to_view_on_site
 
 
-# Django Rest FrameWork
 def weather_def_to_create_a_weather_on_main_page(request):
     url_adress_weather = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=0bb9dcd611790e49af9f17c62bfc2646'
 
@@ -68,9 +67,9 @@ def weather_def_to_create_a_weather_on_main_page(request):
     context = {'all_info': all_cities_list, 'form': form_to_create_a_weather}
     return render(request, 'myprofile/index.html', context)
 
+
+
 # Django Rest FrameWork
-
-
 class My_Profile_Admins(generics.ListCreateAPIView):
     queryset = Myprofile.objects.all()
     serializer_class = MyProfileSerializer
